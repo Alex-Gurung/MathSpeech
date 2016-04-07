2c011f185d701e0353288588e7e3307f5884bc8c
Alex-Gurung <aag1234@gmail.com>
2016-04-06 23:37:08 -0400

    Added nums 1-9

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with sr.Microphone() as source:
 	print("Input equation:")
 	audio = r.listen(source) #Sets a variable called audio to the audio input, to be later interpreted
 
-dict = {"what is": " ", "plus": "+", "added to": "+", "minus":"-", "subracted by": "-", "divided by":"/", "multiplied by" : "*", "x" : "*", "times":"*","parenthesis": "parentheses", "in parentheses" : "(", "end parentheses" : ")", "parentheses" : ")", "negative": "-","to the power" : "**"}
+dict = {"what is": " ", "plus": "+", "added to": "+", "minus":"-", "subracted by": "-", "divided by":"/", "multiplied by" : "*", "x" : "*", "times":"*","parenthesis": "parentheses", "in parentheses" : "(", "end parentheses" : ")", "parentheses" : ")", "negative": "-","to the power" : "**", "one" : "1", "two" : "2", "three" : "3", "four" : "4", "five" : "5", "six" : "6", "seven" : "7", "eight" : "8", "nine" : "9"}
 
 #Below are the try and except blocks for google speech recognition
 
