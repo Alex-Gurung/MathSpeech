4048bb54bed1d121a7a6e38e33487d3f1002efcb
Alex-Gurung <aag1234@gmail.com>
2016-04-16 09:56:38 -0400

    Comment

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ dict = {"what is": " ", "plus": "+", "added to": "+", "minus":"-", "subracted by
 try:
 	equation = r.recognize_google(audio).lower() #Takes whatever the speech interpretor took the input as and makes it lower case to fit the dictionary
 	#equation = raw_input("Input equation: ") # For typing in equation
-	print(equation)
+	print(equation) #Prints the equation, check to see if it recognized your speech correctly
 	for key in dict: #Goes through the equation replacing any phrases with their mathematical equivalents
 		equation = equation.replace(key, dict[key])
 
