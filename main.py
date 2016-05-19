4e9a19dbf1d09a71f09622d28e7fecef03eb3eab
Alex-Gurung <aag1234@gmail.com>
2016-05-19 10:39:17 -0400

    Comment

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sympy import * #Necessary for commented out code, but comment this import i
 r = sr.Recognizer()
 #Code to get speech
 
-with sr.Microphone() as source:
+with sr.Microphone() as source: #Need this block every time you want to input speech
 	#print("Do you want to solve an expression or solve for a variable?") #To be implemented lated with sympy
 	print("Say your equation:")
 	#audio = input("Input your equation")
