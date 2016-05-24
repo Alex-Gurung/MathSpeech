4fd20c56200a1862a7e07e97e8c57b71e83ff9c5
Alex-Gurung <aag1234@gmail.com>
2016-05-24 17:47:46 -0400

    Added more commented code

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ with sr.Microphone() as source: #Need this block every time you want to input sp
 	#print("Do you want to solve an expression or solve for a variable?") #To be implemented lated with sympy
 	print("Say your equation:")
 	#audio = input("Input your equation")
+	#x = Symbol('x')
+	#diff(audio, x)						#For direct derivative typed input
 	audio = r.listen(source) #Sets a variable called audio to the audio input, to be later interpreted
 ######################################
 #EQUATION CODE
