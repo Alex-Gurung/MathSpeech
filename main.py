ec764bc202eed833b4f70e2a00a4f01e9ba624ad
Alex-Gurung <aag1234@gmail.com>
2016-05-31 18:34:28 -0400

    Comment

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ r = sr.Recognizer()
 with sr.Microphone() as source: #Need this block every time you want to input speech
 	#print("Do you want to solve an expression or solve for a variable?") #To be implemented lated with sympy
 	print("Say your equation:")
-	#audio = input("Input your equation")
+	#audio = input("Input your equation") #For text input, can be moved outside of with statement
 	#x = Symbol('x')
 	#diff(audio, x)						#For direct derivative typed input
 	audio = r.listen(source) #Sets a variable called audio to the audio input, to be later interpreted
