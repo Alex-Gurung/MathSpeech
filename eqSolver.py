f9fcfab95388f0539f66fffb4dcb05f8d1e03ad0
Alex-Gurung <aag1234@gmail.com>
2016-06-29 10:02:00 +0100

    Added necessary try and except blocks

--- eqSolver.py
+++ eqSolver.py
@@ -13,3 +13,15 @@ def eqSolver:
 		#x = Symbol('x')
 		#diff(audio, x)						#For direct derivative typed input
 		audio = r.listen(source) #Sets a variable called audio to the audio input, to be later interpreted
+	
+
+	try:
+		equation = r.recognize_google(audio).lower() #Takes whatever the speech interpretor took the input as and makes it lower case to fit the dictionary
+		print(equation) #Prints the equation, check to see if it recognized your speech correctly
+		for i in range(len(keys)): #Goes through the equation replacing any phrases with their mathematical equivalents
+			equation = equation.replace(keys[i], results[i])
+		equation = equation.strip() #Removes unnecessary splaces at ends(shouldn't change anything)
+	except sr.UnknownValueError: #This is the most common error, try audio again, making certain the program can clearly hear you
+	    print("Google Speech Recognition could not understand audio") #The two excepts below are the standard for google speech recognition
+	except sr.RequestError as e: #Make sure to keep these excepts whenever calling recognize_google(audio)
+	    print("Could not request results from Google Speech Recognition service; {0}".format(e))
