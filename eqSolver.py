55449083d907f49ee043ccdb486943b327ae93c8
Alex-Gurung <aag1234@gmail.com>
2016-06-30 11:14:32 -0400

    Added code to solve equation with variable z I don't use x because it replaces x with *

--- eqSolver.py
+++ eqSolver.py
@@ -8,12 +8,12 @@ def eqSolver:
 
 	with sr.Microphone() as source: #Need this block every time you want to input speech
 		#print("Do you want to solve an expression or solve for a variable?") #To be implemented lated with sympy
-		print("Say your equation:")
+		print("Say your equation(with variable z):")
 		#audio = input("Input your equation") #For text input, can be moved outside of with statement
 		#x = Symbol('x')
 		#diff(audio, x)						#For direct derivative typed input
 		audio = r.listen(source) #Sets a variable called audio to the audio input, to be later interpreted
-	
+	z = Symbol('z')
 
 	try:
 		equation = r.recognize_google(audio).lower() #Takes whatever the speech interpretor took the input as and makes it lower case to fit the dictionary
@@ -23,5 +23,9 @@ def eqSolver:
 		equation = equation.strip() #Removes unnecessary splaces at ends(shouldn't change anything)
 	except sr.UnknownValueError: #This is the most common error, try audio again, making certain the program can clearly hear you
 	    print("Google Speech Recognition could not understand audio") #The two excepts below are the standard for google speech recognition
+	    return
 	except sr.RequestError as e: #Make sure to keep these excepts whenever calling recognize_google(audio)
 	    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+	    return
+
+	print(solve(equation, z)) #Solves the equation with a variable 'z' NOTE: must use 'z' and only 'z' as variable
